e1e713818fa8e9af9bd328d1c40046fc49e8f59a
Josh Winter <joshwinter@users.noreply.github.com>
2023-06-23 16:54:49 -0400

    Update AppVersion.mm

--- ios/AppVersion.mm
+++ ios/AppVersion.mm
@@ -19,13 +19,6 @@ RCT_REMAP_METHOD(getBuildNumber,
     resolve(buildNumber);
 }
 
-// Don't compile this code when we build for the old architecture.
-#ifdef RCT_NEW_ARCH_ENABLED
-- (std::shared_ptr<facebook::react::TurboModule>)getTurboModule:
-    (const facebook::react::ObjCTurboModule::InitParams &)params
-{
-    return std::make_shared<facebook::react::NativeAppVersionSpecJSI>(params);
-}
-#endif
+
 
 @end
